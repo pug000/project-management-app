29afd81af8d13ea4fba041d756348ae352d6c985
Roman Troshin <95237510+pug000@users.noreply.github.com>
2022-11-09 12:58:56 +0300

    fix: merge pull request "Add endpoints and methods"
    * feat: add endpoints
    
    * fix: update enum methods

--- src/ts/enums.ts
+++ src/ts/enums.ts
@@ -1,8 +1,21 @@
-enum Endpoints {}
+enum Endpoints {
+  signIn = 'auth/signin/',
+  signUp = 'auth/signup/',
+  users = 'users/',
+  boards = 'boards/',
+  boardsSet = 'boardsSet/',
+  columns = 'columns/',
+  columnsSet = 'columnsSet/',
+  tasks = 'tasks/',
+  tasksSet = 'tasksSet/',
+  file = 'file/',
+  points = 'points/',
+}
 
 enum Methods {
   get = 'GET',
   post = 'POST',
+  patch = 'PATCH',
   put = 'PUT',
   delete = 'DELETE',
 }
