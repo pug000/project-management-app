90cc0f4c77511525e2421c421f09a2ba70219ec7
Roman Troshin <roman.troshin98@gmail.com>
2022-11-10 23:39:37 +0300

    feat: add languages.json

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -2,13 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import languages from 'localization/languages.json';
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {},
+    resources: languages,
     fallbackLng: 'en',
-    debug: true,
     detection: {
       order: ['localStorage'],
       caches: ['localStorage'],
