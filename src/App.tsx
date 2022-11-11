12665e2101a3d3f07177b688acf2f08ae7c78981
Anastasiya Sachko <naztya12323@gmail.com>
2022-11-11 21:20:10 +0300

    refactor: move ErrorBoudary upper and temporally remove Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import HomePage from 'pages/HomePage/HomePage';
@@ -8,23 +8,15 @@ function App() {
   return (
     <>
       <div>Header</div>
-      <Routes>
-        <Route
-          index
-          path="/"
-          element={
-            <ErrorBoundary>
-              <Suspense fallback={<div>Loading...</div>}>
-                <HomePage />
-              </Suspense>
-            </ErrorBoundary>
-          }
-        />
-        <Route path="signin" element={<div>SignIn</div>} />
-        <Route path="signup" element={<div>SignUp</div>} />
-        <Route path="board" element={<div>Board</div>} />
-        <Route path="*" element={<div>NotFound</div>} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="signin" element={<div>SignIn</div>} />
+          <Route path="signup" element={<div>SignUp</div>} />
+          <Route path="board" element={<div>Board</div>} />
+          <Route path="*" element={<div>NotFound</div>} />
+        </Routes>
+      </ErrorBoundary>
       <div>Footer</div>
     </>
   );
