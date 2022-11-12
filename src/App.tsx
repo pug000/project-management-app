c4b3eec1ef20e01ff2e1b48eb1fa2346f6dd7997
Roman Troshin <roman.troshin98@gmail.com>
2022-11-12 17:35:25 +0300

    feat: add sign in on first render component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useCallback, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
+import { useSignInMutation } from 'redux/api/authSlice';
+import { setAuthUser } from 'redux/slices/userSlice';
 
 import AppLayout from 'components/AppLayout/AppLayout';
 import HomePage from 'pages/HomePage/HomePage';
@@ -7,6 +11,27 @@ import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import SignUpPage from 'pages/SignUpPage/SignUpPage';
 
 function App() {
+  const user = useAppSelector((state) => state.user.user);
+  const dispatch = useAppDispatch();
+  const [signIn, { data: authData, isSuccess: isSuccessSignIn }] = useSignInMutation();
+
+  const authUser = useCallback(async () => {
+    if (user) {
+      const { name, ...data } = user;
+      await signIn(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (authData && isSuccessSignIn) {
+      dispatch(setAuthUser(authData));
+    }
+  }, [authData]);
+
+  useEffect(() => {
+    authUser();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
