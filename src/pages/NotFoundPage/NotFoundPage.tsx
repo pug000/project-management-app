51cf6a01a9c15203360a4835c0cb542dda511b0d
Anastasiya Sachko <naztya12323@gmail.com>
2022-11-17 01:18:32 +0300

    feat: create NotFoundPage with styles

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import Button from 'components/Button/Button';
+
+import { MainWrapper, Title } from 'styles/styles';
+
+import { NotFoundText, NotFoundLink, NotFoundIcon } from './NotFoundPage.style';
 
 function NotFoundPage() {
-  return <main>NotFoundPage</main>;
+  const { t } = useTranslation('translation', { keyPrefix: 'notFound' });
+
+  return (
+    <MainWrapper>
+      <NotFoundIcon />
+      <Title>{t('title')}</Title>
+      <NotFoundText>{t('text')}</NotFoundText>
+      <NotFoundLink to="/">
+        <Button type="button" width="210px" text={t('button')} />
+      </NotFoundLink>
+    </MainWrapper>
+  );
 }
 
 export default NotFoundPage;
