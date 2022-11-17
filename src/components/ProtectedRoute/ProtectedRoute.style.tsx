a51d36c8d1579ce166c662f1eeaa0fb55106f187
Roman Troshin <roman.troshin98@gmail.com>
2022-11-18 00:44:54 +0300

    feat: add media queries in protected route component

--- src/components/ProtectedRoute/ProtectedRoute.style.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.style.tsx
@@ -8,11 +8,23 @@ const Container = styled.main`
   justify-content: center;
   align-items: center;
   text-align: center;
+
+  @media (max-width: 768px) {
+    width: 85vw;
+  }
+
+  @media (max-width: 360px) {
+    width: 95vw;
+  }
 `;
 
 const ProtectedTitle = styled.h3`
   font-family: ${(props) => props.theme.fonts.text};
   font-size: ${(props) => props.theme.fontSizes.h3};
+
+  @media (max-width: 500px) {
+    font-size: ${(props) => props.theme.fontSizes.h4};
+  }
 `;
 
 export { Container, ProtectedTitle };
